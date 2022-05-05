563e140f312d9057066016d6a4ff726a9013ebd2
evhen <x500dll@gmai.com>
2022-05-05 20:51:31 +0300

    added btc types public keys

--- src/services/address-generator.ts
+++ src/services/address-generator.ts
@@ -1,4 +1,4 @@
-import { Keypair } from "@solana/web3.js";
+import { Enum, Keypair } from "@solana/web3.js";
 import { mnemonicToSeedSync } from "bip39";
 import EthereumHDKey from "ethereumjs-wallet/dist/hdkey";
 import { inject, injectable } from "inversify";
@@ -11,11 +11,31 @@ const ethBasePath = `m/44'/60'/0'/0`,
     ethBasePathLedger = `m/44'/60'/0'`,
     etcBasePath = `m/44'/61'/0'/0`;
 
+enum BtcPath {
+    Legacy=`m/44'/0'/0'/0`,
+    Ledger=`m/44'/0'`,
+    Segwit=`m/84'/0'/0'`,
+    Electrum=`m/84'/0'`
+}
+
+enum BtcType {
+    Legacy,
+    Segwit,
+    Segwit_p2sh,
+    Electrum
+}
+
 interface GenerateOptions {
     basePath: string
     count?: number
     prefix?: string
     addressNormalizer?: (addr) => string
+}
+
+export interface AddressInfo {
+    address: string
+    path: string
+    note?: string
 }
 
 @injectable()
@@ -32,13 +52,13 @@ export class AddressGenerator {
         return mnemonicToSeedSync(mnemonic);
     }
 
-    _getAddresses(root: EthereumHDKey, opts: GenerateOptions){
+    _getAddresses(root: EthereumHDKey, opts: GenerateOptions): AddressInfo[] {
         const count = opts.count || 3,
             prefix = opts.prefix || '';
 
-        const addresses = [];
+        const addresses:AddressInfo[] = [];
         for(let i = 0; i < count; i++){
-            const wallet = this._getWallet(root, i, opts),
+            const {path,wallet} = this._getWallet(root, i, opts),
                 addrBuffer = wallet.getAddress();
 
             const address = prefix + 
@@ -46,56 +66,93 @@ export class AddressGenerator {
                     ? opts.addressNormalizer(addrBuffer) 
                     : addrBuffer.toString('hex'));
 
-            addresses.push(address)
+            addresses.push({address, path});
         }
         
         return addresses;
     }
 
     _getWallet(root: EthereumHDKey, index, opts: GenerateOptions){
-        return root.derivePath(opts.basePath+`/${index}`).getWallet()
+        const path = `${opts.basePath}/${index}`,
+            wallet = root.derivePath(path).getWallet();
+
+        return { path, wallet}
     }
 
     _getSolanaAddr(seed, walletIndex){
-        const keys = derivePath(`m/44'/501'/${walletIndex}'/0'`, seed.toString('hex')),
-            pair = Keypair.fromSeed(keys.key);
+        const path = `m/44'/501'/${walletIndex}'/0'`,
+            keys = derivePath(path, seed.toString('hex')),
+            pair = Keypair.fromSeed(keys.key),
+            address = pair.publicKey.toString();
 
-        return pair.publicKey.toString();
+        return {path, address};
     }
 
-    getEthAddresses(root: EthereumHDKey, count: number){
+    getEthAddresses(root: EthereumHDKey, count: number):AddressInfo[]{
         return this._getAddresses(root, {basePath: ethBasePath, count, prefix:'0x'});
     }
     
-    getEthLedgerAddresses(root: EthereumHDKey, count: number){
+    getEthLedgerAddresses(root: EthereumHDKey, count: number):AddressInfo[]{
         return this._getAddresses(root, { basePath: ethBasePathLedger, count, prefix:'0x'});
     }
 
-    getEtcAddresses(root: EthereumHDKey, count: number){
+    getEtcAddresses(root: EthereumHDKey, count: number):AddressInfo[]{
         return this._getAddresses(root, {basePath: etcBasePath, count, prefix:'0x'})
     }
 
-    getSolanaAddresses(mnemonic: string, count: number){
-        const addresses = [],
+    getSolanaAddresses(mnemonic: string, count: number):AddressInfo[]{
+        const addresses:AddressInfo[] = [],
             seed = this.getSeed(mnemonic)
 
         for(let i = 0; i < count; i++){
-            addresses.push(this._getSolanaAddr(seed, i))
+            addresses.push(this._getSolanaAddr(seed, i));
         }
 
         return addresses;
     }
 
-    getBTCAddress(mnemonic: string, count: number){
-        const seed = this.getSeed(mnemonic),
-            root = fromSeed(seed),
-            addresses = [];
+    getBtcAddresses(seed: Buffer, count:number):AddressInfo[]{
+        const paths = [BtcPath.Electrum,BtcPath.Ledger,BtcPath.Legacy,BtcPath.Segwit],
+            types = [BtcType.Legacy,BtcType.Segwit,BtcType.Segwit_p2sh];
+
+        const addresses:AddressInfo[] = [];
+
+        for(let i in types){
+            for(let j in paths){
+                for(let index = 0; index < count; index++){
+                    addresses.push(...this._getBTCAddress(seed, count, types[i], paths[j]))
+                }
+            }
+        }
+
+        return addresses;
+    }
+
+    _getBTCAddress(seed: Buffer, count: number, type: BtcType, basePath: string):AddressInfo[]{
+        const root = fromSeed(seed),
+            addresses:AddressInfo[] = [];
 
         for(let i = 0; i < count; i++){
-            const addrNode = root.derivePath(`m/44'/0'/0'/0/${i}`),
-                p2pkh = btcjs.payments.p2pkh({pubkey: addrNode.publicKey});
+            const path = `${basePath}/${i}`,
+                addrNode = root.derivePath(path);
 
-            addresses.push(p2pkh.address)
+                let address:string;
+                let note: string;
+                if(type == BtcType.Segwit){
+                    address = btcjs.payments.p2wpkh({pubkey: addrNode.publicKey}).address;
+                    note = 'segwit p2wpkh'
+                } else if(type == BtcType.Segwit_p2sh){
+                    address = btcjs.payments.p2sh({
+                        redeem: btcjs.payments.p2wpkh({pubkey: addrNode.publicKey})
+                    }).address
+                    note = 'segwit p2sh'
+                } else {
+                    address = btcjs.payments.p2pkh({pubkey: addrNode.publicKey}).address;
+                    note = 'p2pkh'
+                }
+                
+
+            addresses.push({address, path, note})
         }
 
         return addresses;
--- src/services/mnemonic-response-generator.ts
+++ src/services/mnemonic-response-generator.ts
@@ -1,33 +1,35 @@
 import { inject, injectable } from "inversify";
 import { IDS } from "../types";
-import { AddressGenerator } from "./address-generator";
+import { AddressGenerator, AddressInfo } from "./address-generator";
 
 @injectable()
 export class MnemonicResponseGenerator {
     @inject(IDS.SERVICE.AddressGenerator) _addressGenerator:AddressGenerator
 
     getMessage(mnemonic:string, addressCount:number){
-        const root = this._addressGenerator.getRoot(mnemonic),
-                msg = [];
+        const seed = this._addressGenerator.getSeed(mnemonic),
+            root = this._addressGenerator.getRoot(mnemonic),
+            msg = [],
+            foramtAddress = (addrInfo:AddressInfo)=> `${addrInfo.address} (${addrInfo.path}${addrInfo.note ? (' ' + addrInfo.note) :''})`;
             
             msg.push('Ethereum:')
-            msg.push(this._addressGenerator.getEthAddresses(root,addressCount).join("\n"))
+            msg.push(this._addressGenerator.getEthAddresses(root,addressCount).map(foramtAddress).join("\n"))
             msg.push('')
 
             msg.push('Ledger:')
-            msg.push(this._addressGenerator.getEthLedgerAddresses(root,addressCount).join("\n"))
+            msg.push(this._addressGenerator.getEthLedgerAddresses(root,addressCount).map(foramtAddress).join("\n"))
             msg.push('')
 
             msg.push('Ethereum Classic:')
-            msg.push(this._addressGenerator.getEtcAddresses(root,addressCount).join("\n"))
+            msg.push(this._addressGenerator.getEtcAddresses(root,addressCount).map(foramtAddress).join("\n"))
             msg.push('')
 
             msg.push('Solana:')
-            msg.push(this._addressGenerator.getSolanaAddresses(mnemonic,addressCount).join("\n"))
+            msg.push(this._addressGenerator.getSolanaAddresses(mnemonic,addressCount).map(foramtAddress).join("\n"))
             msg.push('')
 
             msg.push('Bitcoin:')
-            msg.push(this._addressGenerator.getBTCAddress(mnemonic,addressCount).join("\n"))
+            msg.push(this._addressGenerator.getBtcAddresses(seed,1).map(foramtAddress).join("\n"))
             msg.push('') 
 
             return msg.join('\n')
